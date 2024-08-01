63a07c1ddb77d2305266c3f520ffc6412155df2c
HiiWee <wpdlzhf159@gmail.com>
2024-08-01 17:30:23 +0900

    feat: String 덧셈 예제 추가

---
+++ src/main/java/com/project/StringPlus.java
@@ -0,0 +1,13 @@
+package com.project;
+
+public class StringPlus {
+
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+
+        String c = a + b;
+
+        System.out.println(c);
+    }
+}
